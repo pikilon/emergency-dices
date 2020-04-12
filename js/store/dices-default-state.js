68f4a3d387e62b37a107df0765066e4c26678052
Pikilon <pikilon@gmail.com>
2020-04-12 10:34:44 +0200

    adds scrabble

--- js/store/dices-default-state.js
+++ js/store/dices-default-state.js
@@ -13,5 +13,23 @@ export default {
       {content: '1-3', type: SIDES_TYPES.NUMBER_INTERVAL},
       {content: '💗💥💲', type: SIDES_TYPES.SYMBOL},
     ]
-  }
+  },
+  'alphabet': {
+    title: 'Alphabet',
+    sides: [
+      {content: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', type: SIDES_TYPES.SYMBOL},
+    ]
+  },
+  'vocals': {
+    title: 'Vocals',
+    sides: [
+      {content: 'AEIOU', type: SIDES_TYPES.SYMBOL},
+    ]
+  },
+  'consonants': {
+    title: 'Consonants',
+    sides: [
+      {content: 'BCDFGHJKLMNPQRSTVWXYZ', type: SIDES_TYPES.SYMBOL},
+    ]
+  },
 }
--- js/store/games-sets-default-state.js
+++ js/store/games-sets-default-state.js
@@ -8,5 +8,14 @@ export default {
     title: 'King of Tokyo',
     slug: 'king-of-tokyo',
     dices: [{slug: 'king-of-tokyo', amount: 5}]
-  }
+  },
+  'scrabble': {
+    title: 'Scrabble',
+    slug: 'scrabble',
+    dices: [
+      {slug: 'vocals', amount: 2, color: 'blue'},
+      {slug: 'consonants', amount: 3},
+      {slug: 'alphabet', amount: 1, color: 'red'},
+    ]
+  },
 }
